2f9114b0f07c64cd9a361a2828e033ea313d1ac5
Omar Trejo <otrenav@gmail.com>
2018-02-05 15:06:51 -0600

    Dinámica de filtros dobles para día/hora en ARES

--- public/static/js/ares-main.js
+++ public/static/js/ares-main.js
@@ -3,9 +3,107 @@
 // NOTE: Global-space objects: `HEATMAPS`, `MAP`, `updateBoxInts()`
 //
 
+var DAY_SELECTED = '';
+
 var pageInit = function() {
     insertOptions();
     initializeHeatmaps();
+};
+
+var heatmapToggleDay = function(day, div) {
+    if (DAY_SELECTED !== '') {
+        resetDaySelection();
+    }
+    if (DAY_SELECTED === day) {
+        $(div).removeClass("option-enabled");
+        heatmapHoursNone();
+        DAY_SELECTED = '';
+    } else {
+        $(div).addClass("option-enabled");
+        DAY_SELECTED = day;
+        heatmapHoursAll();
+    }
+    updateBoxInts([]);
+};
+
+var heatmapToggleTime = function(time, div) {
+    if (DAY_SELECTED === '') {
+        warning(true);
+    } else {
+        warning(false);
+        if ($(div).hasClass("option-enabled")) {
+            $(div).removeClass("option-enabled");
+            heatmapUpdateDayTime(DAY_SELECTED, time, null);
+        } else {
+            heatmapUpdateDayTime(DAY_SELECTED, time, MAP);
+            $(div).addClass("option-enabled");
+        }
+        updateBoxInts([]);
+    }
+};
+
+var heatmapHoursAll = function() {
+    if (DAY_SELECTED === '') {
+        warning(true);
+    } else {
+        warning(false);
+        heatmapsUpdateAllHoursForDay(DAY_SELECTED, MAP);
+        $("div.hour-option").addClass("option-enabled");
+        updateBoxInts([]);
+    }
+};
+
+var heatmapHoursNone = function() {
+    warning(false);
+    $("div.hour-option").removeClass("option-enabled");
+    heatmapsUpdateAllHoursForDay(DAY_SELECTED, null);
+    updateBoxInts([]);
+};
+
+var heatmapsUpdateAllHoursForDay = function(day, obj) {
+    for (var time in HEATMAPS[day]) {
+        if (HEATMAPS[day].hasOwnProperty(time)) {
+            heatmapUpdateDayTime(day, time, obj);
+        }
+    }
+};
+
+var heatmapUpdateDayTime = function(day, time, obj) {
+    HEATMAPS[day][time].googleMapsObject.setMap(obj);
+};
+
+var resetDaySelection = function() {
+    warning(false);
+    heatmapHoursNone();
+    $("div.option").removeClass("option-enabled");
+};
+
+var initializeHeatmaps = function() {
+    var heatmapData;
+    for (var day in HEATMAPS) {
+        if (HEATMAPS.hasOwnProperty(day)) {
+            for (var time in HEATMAPS[day]) {
+                if (HEATMAPS[day].hasOwnProperty(time)) {
+                    heatmapData = [];
+                    var coordsList = HEATMAPS[day][time].coords;
+                    for(var i = 0; i < coordsList.length; i++) {
+                        heatmapData.push(new google.maps.LatLng(
+                            coordsList[i][0], coordsList[i][1]
+                        ));
+                    }
+                    HEATMAPS[day][time].googleMapsObject = (
+                        new google.maps.visualization.HeatmapLayer({
+                            data: heatmapData,
+                            dissipating: true
+                        })
+                    );
+                    HEATMAPS[day][time].googleMapsObject.setOptions({
+                        radius: HEATMAPS[day][time].googleMapsObject.get('120')
+                    });
+                }
+            }
+        }
+    }
 };
 
 var insertOptions = function() {
@@ -13,90 +111,42 @@ var insertOptions = function() {
     insertTimeOptions();
 };
 
-var dayOptionHTML = function(selector) {
-};
-
-var timeOptionHTML = function(selector) {
-    return `
-        <div class='col-lg-6 col-2 option' onclick="heatmapToggle('${selector}', this);">
-            <span>${selector}</span>
-        </div>
-    `;
-};
-
 var insertDayOptions = function() {
+    for (var day in HEATMAPS) {
+        if (HEATMAPS.hasOwnProperty(day)) {
+            $('#day-options').append(dayOptionHTML(day));
+        }
+    }
 };
 
 var insertTimeOptions = function() {
-    for (var selector in HEATMAPS) {
-        if (HEATMAPS.hasOwnProperty(selector)) {
+    for (var selector in HEATMAPS.Lunes) {
+        if (HEATMAPS.Lunes.hasOwnProperty(selector)) {
             $('#time-options').append(timeOptionHTML(selector));
         }
     }
 };
 
-var heatmapsUpdateAll = function(obj) {
-    for (var selector in HEATMAPS) {
-        if (HEATMAPS.hasOwnProperty(selector)) {
-            heatmapUpdate(selector, obj);
-        }
-    }
+var dayOptionHTML = function(day) {
+    return `
+        <div class='col-lg-12 col-2 option' onclick="heatmapToggleDay('${day}', this);">
+            <span>${day}</span>
+        </div>
+    `;
 };
 
-var heatmapHoursAll = function() {
-    $("div.option").addClass("active-option");
-    heatmapsUpdateAll(MAP);
-    updateBoxInts([]);
+var timeOptionHTML = function(selector) {
+    return `
+        <div class='col-lg-6 col-2 hour-option' onclick="heatmapToggleTime('${selector}', this);">
+            <span>${selector}</span>
+        </div>
+    `;
 };
 
-var heatmapHoursNone = function() {
-    $("div.option").removeClass("active-option");
-    heatmapsUpdateAll(null);
-    updateBoxInts([]);
-};
-
-var heatmapDaysAll = function() {
-    updateBoxInts([]);
-};
-
-var heatmapDaysNone = function() {
-    updateBoxInts([]);
-};
-
-var heatmapToggle = function(selector, div) {
-    if ($(div).hasClass("active-option")) {
-        heatmapUpdate(selector, null);
-        $(div).removeClass("active-option");
+var warning = function(boolean) {
+    if (boolean) {
+        $('#select-a-day-warning').css('display', 'inline');
     } else {
-        heatmapUpdate(selector, MAP);
-        $(div).addClass("active-option");
-    }
-    updateBoxInts([]);
-};
-
-var heatmapUpdate = function(selector, obj) {
-    HEATMAPS[selector].googleMapsObject.setMap(obj);
-};
-
-var initializeHeatmaps = function() {
-    for (var selector in HEATMAPS) {
-        if (HEATMAPS.hasOwnProperty(selector)) {
-            var heatmapData = [];
-            var coordsList = HEATMAPS[selector].coords;
-            for(var i = 0; i < coordsList.length; i++) {
-                heatmapData.push(new google.maps.LatLng(
-                    coordsList[i][0], coordsList[i][1]
-                ));
-            }
-            HEATMAPS[selector].googleMapsObject = (
-                new google.maps.visualization.HeatmapLayer({
-                    data: heatmapData,
-                    dissipating: true
-                })
-            );
-            HEATMAPS[selector].googleMapsObject.setOptions({
-                radius: HEATMAPS[selector].googleMapsObject.get('120')
-            });
-        }
+        $('#select-a-day-warning').css('display', 'none');
     }
 };
